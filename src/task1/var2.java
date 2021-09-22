24106d994448cc410137616d74b1479288111267
Антон <antonuniverse7@gmail.com>
2021-09-22 17:26:17 +0300

    Second commit

---
+++ src/task1/var2.java
@@ -0,0 +1,12 @@
+package task1;
+
+import java.util.Scanner;
+
+public class var2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        for (int i = 0; i < 10; i++) {
+            System.out.println(scan.nextLine());
+        }
+    }
+}
