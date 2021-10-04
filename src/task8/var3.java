c2a68230866ace893377242ab1273d8bd78f76dc
Антон <antonuniverse7@gmail.com>
2021-10-04 18:59:49 +0300

    Image Commit

--- src/task8/var3.java
+++ src/task8/var3.java
@@ -1,4 +1,11 @@
 package task8;
 
+import javax.swing.*;
+import java.util.Scanner;
+
 public class var3 {
-}
+    public static void main(String[] argc) {
+        JFrame window = new JFrame("AnimationFrame");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //программа закончит работу при закрытии окна
+        window.setSize(300, 300);
+    }
