7c55023446a9dd264b19a9f182fba72da19d8d11
Антон <antonuniverse7@gmail.com>
2021-10-12 11:26:51 +0300

    Animation Commit

--- src/task8/var3.java
+++ src/task8/var3.java
@@ -2,10 +2,31 @@ package task8;
 
 import javax.swing.*;
 import java.util.Scanner;
-
 public class var3 {
     public static void main(String[] argc) {
-        JFrame window = new JFrame("AnimationFrame");
-        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //программа закончит работу при закрытии окна
-        window.setSize(300, 300);
-    }}
+        String[] path = new String[8];
+        Scanner scan = new Scanner(System.in);
+        JFrame frame = new JFrame("PictureFrame");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300, 300);
+
+        for (int i = 0; i < 8; i++)
+        {
+            //path[i] = scan.nextLine();
+            path[i]="C:\\Users\\Антон\\Desktop\\Anim\\"+String.valueOf(i)+".jpg";
+        }
+
+        while(true) {
+            for (int i = 0; i < path.length; i++) {
+                ImageIcon img = new ImageIcon(path[i]);
+                JLabel label = new JLabel(img);
+                frame.add(label);
+                frame.setVisible(true);
+                try {
+                    Thread.sleep(100);
+                } catch(InterruptedException ex) {}
+                frame.getContentPane().removeAll();
+            }
+        }
+    }
+}
