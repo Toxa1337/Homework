9730024a2bbe9a2828e251b421032b8a7936b0c3
Антон <antonuniverse7@gmail.com>
2021-10-27 16:39:47 +0300

    Task 12 var 1 commit

--- src/task12/var1.java
+++ src/task12/var1.java
@@ -2,5 +2,54 @@ package task12;
 
 //Напишите тестовый класс, который создает массив класса Student и сортирует массив iDNumber вставками.
 
+import java.lang.*;
+import java.util.Arrays;
+
 public class var1 {
+
+    public static void sort(int[] idNumber)
+    {
+        System.out.println(Arrays.toString(idNumber));
+        for (int left = 1; left < idNumber.length; left++) {
+            int value = idNumber[left];
+            int i = left - 1;
+            for (; i >= 0; i--) {
+                if (value < idNumber[i]) {
+                    idNumber[i + 1] = idNumber[i];
+                } else {
+                    break;
+                }
+            }
+            idNumber[i + 1] = value;
+        }
+        System.out.println(Arrays.toString(idNumber));
+    }
+
+    public static void main(String[] args)
+    {
+        Student[] stud = new Student[10];
+        int[] idNumber = new int[stud.length];
+        for (int i = 0; i < stud.length; i++)
+        {
+            int ranNum = (int) (1 + Math.random()*700);
+            stud[i] = new Student(ranNum);
+        }
+        for(int i = 0; i < stud.length; i++)
+        {
+            idNumber[i] = stud[i].getId();
+        }
+        sort(idNumber);
+    }
+}
+
+class Student
+{
+    private int id;
+    public Student(int id) {
+        this.id = id;
+    }
+    public int getId()
+    {
+        return id;
+    }
 }
