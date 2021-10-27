a8dfc2ac1a003bda2b56ae303bc421814c1d4481
Антон <antonuniverse7@gmail.com>
2021-10-27 16:00:26 +0300

    Task 15 var 3 commit

--- src/task15/var3.java
+++ src/task15/var3.java
@@ -1,4 +1,28 @@
 package task15;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class var3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String in;
+        boolean check = true;
+        try {
+            FileWriter fW = new FileWriter("src/task15/test.txt");
+            System.out.println("Enter message (for end input type END in empty string):");
+            in = sc.nextLine();
+            do {
+                fW.write(in + "\r\n");
+                in = sc.nextLine();
+                if (in.endsWith("END")){
+                    check=false;
+                }
+            } while (check);
+            fW.close();
+        } catch (IOException e) {
+            System.out.println("Error: file doesn't exist, or input problem");
+        }
+    }
 }
