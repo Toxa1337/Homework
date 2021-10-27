49f9396fdc58a165db6765e94ffe7b241dd80c3f
Антон <antonuniverse7@gmail.com>
2021-10-27 15:30:27 +0300

    Task 15 var 2 commit

--- src/task15/var2.java
+++ src/task15/var2.java
@@ -1,4 +1,18 @@
 package task15;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class var2 {
+    public static void main(String[] args) {
+        try {
+            Scanner fRead = new Scanner(new File("src/task15/test.txt"));
+            System.out.println("File content:");
+            while (fRead.hasNextLine()) System.out.println(fRead.nextLine());
+            fRead.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Error: file doesn't exist, or output problem");
+        }
+    }
 }
